2fa74d5e58014616050a54dbd9ea8cd6d7d98953
Tom Narock <tnarock@ndm.edu>
2019-06-14 15:58:40 -0400

    Updated code to work with dataframes

--- cosPreprints.py
+++ cosPreprints.py
@@ -1,8 +1,47 @@
 import os
+import sys
+import utils
 
-# get the configuration parameters from environment variables
-cosApiToken = os.environ['cosApiToken']   # for accessing COS API 
-emailAddress = os.environ['emailAddress'] # for accessing Unpaywall API
+def main():
 
-print( cosApiToken )
-print( emailAddress )
+   # get the configuration parameters from environment variables
+   cosApiToken = os.environ['cosApiToken']   # for accessing COS API 
+   emailAddress = os.environ['emailAddress'] # for accessing Unpaywall API
+
+   # command line inputs
+   # downloadDir - directory to download papers to, also where logs are stored
+   downloadDir = sys.argv[1]
+   # print out status messages as we go along (True or False)
+   verbose = sys.argv[2]
+   # start date - YYYY-MM-DD we should start the index from
+   startDate = sys.argv[3]
+   endDate = sys.argv[4]
+   # log file for the peer reviewed article data
+   peerReviewLog = sys.argv[5]
+
+   # seperator for log file
+   s1 = ';'
+
+   # the API returns all preprints that were created
+   # some papers may not be available for download
+   # due to moderation problems or retraction
+   # keep track of how many preprints we actually get
+   numPreprints = 0
+
+   # check that the download directory includes 
+   # the trailing /
+   lc = downloadDir[-1]
+   if (lc != '/'):
+     downloadDir += '/'
+
+   # preprint provider
+   provider = 'eartharxiv'
+
+   # set up log files based on provider
+   log = downloadDir + provider + '.log'
+
+   # get the papers
+   preprints = utils.callOsfApi(cosApiToken, provider, startDate, endDate, verbose)
+   print( len(preprints) )
+
+main()
--- utils.py
+++ utils.py
@@ -3,38 +3,110 @@ import requests
 import pandas as pd
 import urllib.request
 
-def createDataframe( preprints ):
+####################
+## URLs for OSF API
+####################
+
+# API URL to list all preprint providers
+api_url_providers = "https://api.osf.io/v2/preprint_providers"
+
+# API URL for search
+api_url_search = "https://api.osf.io/v2/preprints/?filter[provider]=" 
+
+####################
+####################
+
+def callOsfApi( cosApiToken, provider='eartharxiv', startDate='', endDate='', verbose=True ):
+
+   # List to hold all our manuscripts
+   manuscripts = []
+
+   # set up the search URL for this provider
+   apiUrl = api_url_search + provider
+
+   # Are we filtering by date?
+   if ( startDate != '' ):
+       apiUrl += '&filter[date_created][gte]=' + startDate 
+   if ( endDate != '' ):
+       apiUrl += '&filter[date_created][lte]=' + endDate 
+
+   # Set up the headers to be sent as part of every API request
+   headers = {'Content-Type': 'application/json',
+             'Authorization': 'Bearer {0}'.format(cosApiToken)}
+
+   # Send our request to the search API
+   response = queryAPI(apiUrl, headers)
+
+   # Check the response status code, 200 indicates everything worked as expected
+   if response.status_code == 200:
+
+       # Extract the JSON data from the response
+	   json_object = getJSON( response ) 
+
+       # Total number of manuscripts in the results
+	   total_manuscripts = json_object['links']['meta']['total']
+	   if (verbose): print("Total manuscripts returned:", total_manuscripts)
+
+	   # Status update
+	   page = 1
+	   if (verbose): print("Working on results page", page)
+
+       # Parse all the manuscripts in the response (the current 'page' of results)
+	   ms = parseManuscripts( provider, json_object, headers, verbose )
+	   manuscripts.extend( ms )
+
+       # The API returns 10 preprints per "page". We need to look at the Links
+       # data to see if there are additional pages. 
+	   next = json_object['links']['next']
+
+       # Send a request to the search API, this time for the next page
+	   while( next != None ):
+		   page +=1
+		   if (verbose): print("Working on results page", page)
+		   nextResponse = queryAPI(next, headers)
+		   json_object = getJSON( nextResponse ) 
+		   ms = parseManuscripts( provider, json_object, headers )
+		   manuscripts.extend( ms )
+		   next = json_object['links']['next']
+
+   else:
+
+       # Something went wrong with the API call/response
+       print( "Error connecting to API, HTTP status code is: ", response.status_code )
+
+   return manuscripts
+
+def createDataframe( manuscripts ):
 
   # Create DataFrame
-  df = pd.DataFrame( for preprint in preprints )
+  return pd.DataFrame( manuscripts )
  
-  return df
+def createManuscriptDict():
 
-def preprintDict():
-
-	d = {
-                        "identifier": '',
-                        "preprintProvider": '',
-                        "doi": '',
-                        "title": '',
-                        "abstract": '', 
-                        "status": '',
-                        "downloadURL": '', 
-                        "downloadCount": 0,
+	d = { "cosID": '', 
+	      "identifier": '',
+		  "identifierType": '',
+          "preprintProvider": '',
+          "doi": '',
+          "title": '',
+          "abstract": '', 
+          "status": '',
+          "webURL": '',
+          "downloadURL": '', 
+          "downloadCount": 0,
                 
-                        "dateModified": '', 
-                        "datePublished": '', 
+          "dateModified": '', 
+          "datePublished": '', 
 
-                        "doajURL": '',
-                        "doajPublisher": '', 
-                        "doajDoi": '',
-                        "doajPublicationDate": ''
+          "doajURL": '',
+          "doajPublisher": '', 
+          "doajDoi": '',
+          "doajPublicationDate": '',
                 
-                        "authors": [],
-                        "keywords": []
-     	}
+          "authors": [],
+          "keywords": [] }
 
-     return d
+	return d
 
 # Helper function to download a file from a URL
 def download( url, localFile ):
@@ -69,23 +141,28 @@ def getJSON( response ):
 	return json_object
 
 # Helper function to parse JSON response and look for citation data
-def parseCitation ( jsonData, preprint ):
+def parseCitation ( jsonData, manuscript ):
 	
+	authors = []
 	data = jsonData['data']['attributes'] 
-	preprint.title = data['title'] 
+	manuscript['title'] = data['title'] 
 	for a in range( len(data['author']) ):
 		given = data['author'][a]['given']
 		family = data['author'][a]['family']
 		name = given.strip() + " " + family.strip()
-		preprint.authors.append(name)
+		authors.append(name)
+	manuscript['authors'] = authors
+	return manuscript
 
 # Helper function to parse JSON response and look for identifier data
-def parseIdentifier ( jsonData, preprint ):
+def parseIdentifier ( jsonData, manuscript ):
 
-	num_elements = len( jsonData['data'] )
-	for j in range( num_elements ):
-		data = jsonData['data'][j]
-		preprint.parseIdentifierData( data )
+	data = jsonData['data'][0]['attributes']
+	category = data['category'] 
+	value = data['value']
+	manuscript['identifier'] = value
+	manuscript['identifierType'] = category
+	return manuscript
 
 # Funtion to loop over all download counts and extract data
 def parseDownloadCounts( json_object ):
@@ -110,9 +187,12 @@ def parseDownloadCounts( json_object ):
         return dois, dates, downloads
 
 # Function to loop over all preprints in the response and parse their data
-def parsePreprints( preprints, json_object, headers, verbose=True ):
+def parseManuscripts( provider, json_object, headers, verbose=True ):
 
-	# Loop over all the response preprints
+    # list to hold all the manuscripts
+	manuscripts = []
+
+	# Loop over all the response manuscripts
 	for i in range( len(json_object['data']) ):
 	
 		# Response is comprised of Relationships, Links, and Attributes
@@ -120,87 +200,86 @@ def parsePreprints( preprints, json_object, headers, verbose=True ):
 		rel = json_object['data'][i]['relationships']
 		attr = json_object['data'][i]['attributes']
 
-		# Links contains references to peer reviewed paper (if available) 
+		# Links also contains references to peer reviewed paper (if available) 
 		links = json_object['data'][i]['links']
+		html_link = links['html']
+		download_link = html_link + "/download"
 		
-		# Also need to extract scalar values like id and construct the download link
-		id = json_object['data'][i]['id']
-		html_link = "https://eartharxiv.org/" + id 
-		download_link = "https://eartharxiv.org/" + id + "/download"
-		if ( verbose ): print( "Working on preprint ", len(preprints)+1 ) # should be 1 when len=0, thus the +1
-		
-		# Create a Preprint object to store all the information on this preprint
-		preprint = Preprint()
+		# Create a manuscript dictionary to store all the information 
+		manuscript = createManuscriptDict()
+		manuscript['cosID'] = json_object['data'][i]['id']
+		manuscript['webURL'] = html_link
+		manuscript['downloadURL'] = download_link
+		manuscript['provider'] = provider
 
-		# Add the id and download_link values to our object
-		preprint.setScalars( id, html_link, download_link )
-
-		# Parse the Attributes data for this preprint
-		preprint.parseAttrData( attr )
+		# Parse the Attributes data for this manuscript
+		manuscript = parseAttrData( attr, manuscript )
 
 		# The Relationships data has links to more information
-		# Use our helper function to extract those links and put them in our preprint object
-		preprint.parseRelData( rel )
+		identifiersLink, citationLink = parseRelData( rel )
 
 		# Now that we have the identifiers link (from the Relationships data), send it back to the API
 		# to get the Identifier JSON and extract the actual DOI identifier
-		if ( preprint.identifiersLink != '' ):
+		if ( identifiersLink != '' ):
 		  
-			response2 = queryAPI( preprint.identifiersLink, headers )
+			response2 = queryAPI( identifiersLink, headers )
 			if response2.status_code == 200:
 				json_object2 = getJSON( response2 )
-				parseIdentifier( json_object2, preprint )
+				manuscript = parseIdentifier( json_object2, manuscript )
 			else:
 				print( "Error parsing Identifier Link, HTTP status code is: ", response2.status_code )
 
 			# Do the same with the citation link to get all the authors
 		
-			response2 = queryAPI( preprint.citationLink, headers )
+			response2 = queryAPI( citationLink, headers )
 			if response2.status_code == 200:
 				json_object2 = getJSON( response2 )
-				parseCitation( json_object2, preprint )
+				manuscript = parseCitation( json_object2, manuscript )
 			else:
 				print( "Error parsing Citation Link, HTTP status code is: ", response2.status_code )
 
-			# Parse the Links to look for peer reviewed version of paper
-			preprint.parseLinkData( links )		
+			# check with unpaywall to see if there is a DOAJ version of this paper
+			#unpaywall( links )		
 
-		# Add the current preprint to our list of preprints
-		preprints.append( preprint )
+		# Add the current manuscript to our list 
+		manuscripts.append( manuscript )    
 
-def parseIdentifierData( self, idJSON ):
+	return manuscripts
 
-    if ( idJSON['attributes']['category'] == 'doi' ):
-        self.doi = 'https://dx.doi.org/' + idJSON['attributes']['value']
+def parseAttrData( attr, manuscript ):
 
-def parseAttrData( self, attr ):
+    keywords = []
 
-    self.date_last_transitioned = attr['date_last_transitioned']
-    self.date_modified = attr['date_modified']
-    self.description = attr['description']
-    self.date_published = attr['date_published']
-    self.status = attr['reviews_state']
-    self.preprint_doi_created = attr['preprint_doi_created'] 
+    manuscript['dateModified'] = attr['date_modified']
+    manuscript['abstract'] = attr['description']
+    manuscript['status'] = attr['reviews_state']
+    # there is also 'date_published' which is the date/time received by COS
+    # but a doi may not be assigned right away
+    # we use the date/time doi was assigned as the publication date
+    manuscript['datePublished'] = attr['preprint_doi_created'] 
     n = len( attr['subjects'] )
     for i in range(n):
         keyword = attr['subjects'][i]
         n2 = len( keyword )
         for j in range(n2):
-            # API returns the full keyword taxonomy, 
+            # API returns the full keyword hierarchy, 
             # i.e Physical Sciences and Mathematics -> Earth Sciences -> Geology
             # We don't need to capture this each time
             # If we already have Physical Sciences and Mathematics -> Earth Sciences then just grab Geology
-            if keyword[j]['text'] not in self.keywords:  
-                self.keywords.add( keyword[j]['text'] )
+            if keyword[j]['text'] not in keywords:  
+                keywords.append( keyword[j]['text'] )
+    manuscript['keywords'] = keywords
 
+    return manuscript
 
-def parseRelData( self, rel ):
+def parseRelData( rel ):
                         
     # make sure the links entry is available
     test = rel['identifiers']
     if ( 'links' in test ):
-        self.identifiersLink = rel['identifiers']['links']['related']['href'] # DOI and other identifiers
-        self.citationLink = rel['citation']['links']['related']['href'] # citation details
+        identifiersLink = rel['identifiers']['links']['related']['href'] # DOI and other identifiers
+        citationLink = rel['citation']['links']['related']['href'] # citation details
     else:
-        self.identifiersLink = ''
-        self.citationLink = ''
+        identifiersLink = ''
+        citationLink = ''
+    return identifiersLink, citationLink
